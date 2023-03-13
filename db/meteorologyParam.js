86952e9dcdb9d8a82088a70c452ffbea015d48b8
m673002010 <m673002010@163.com>
2023-03-14 01:07:52 +0800

    MeteorologyParam

---
+++ db/meteorologyParam.js
@@ -0,0 +1,17 @@
+/**
+{
+    companyNum: String,
+    date: Date,
+    temperature: String,
+    humidity: String,
+    barometricPressure: String,
+    windDirection: String,
+    windSpeed: String,
+    createDate: Date,
+    createUser: String,
+    editDate: Date,
+    editUser: String,
+}
+ */
+const meteorologyParamCollection = mongoDb.collection('meteorologyParam')
+module.exports = meteorologyParamCollection
