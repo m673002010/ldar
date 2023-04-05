67f58dc309ba30d271603f56042fa3e0561f70cf
m673002010 <m673002010@163.com>
2023-04-05 11:55:51 +0800

    componentInfoLedger

--- controller/cilController.js
+++ controller/cilController.js
@@ -1,5 +1,6 @@
 const componentCollection = require('../db/component')
 const componentTypeCollection = require('../db/componentType')
+const lodash = require('lodash')
 
 async function componentInfoLedger (ctx, next) {
     try {
@@ -29,10 +30,16 @@ async function componentInfoLedger (ctx, next) {
         if (status) query.status = status
         if (medium) query.medium = medium
 
-        console.log('============', query)
+        // 根据动静密封筛选
+        const q = {}
+        if (sealPointType) q.sealPointType = sealPointType
+        let componentTypes = await componentTypeCollection.find(q).toArray()
+        componentTypeArr = lodash.map(componentTypes, 'componentType')
 
-        const componentData = await componentCollection.find(query).limit(+pageSize).skip((+currentPage - 1) * pageSize).toArray()
-        const total = await componentCollection.find(query).count()
+        let componentData = await componentCollection.find(query).toArray()
+        componentData = lodash.filter(componentData, c => { return componentTypeArr.includes(c.componentType) })
+        const total = componentData.length
+        componentData = componentData.slice((currentPage-1) * pageSize, currentPage * pageSize)
         
         ctx.body = { code: 0 , message: '查询组件成功', data: { componentData, total } }
     } catch (err) {
@@ -44,17 +51,39 @@ async function componentInfoLedger (ctx, next) {
 async function exportComponentInfoLedger (ctx, next) {
     try {
         const { companyNum } = ctx.userInfo
-        const { device = '', area = '', equipment = '' } = ctx.request.query
+        const { 
+            device = '', 
+            area = '', 
+            equipment = '', 
+            label = '',
+            unreachable = '',
+            componentType = '',
+            sealPointType = '',
+            status = '',
+            medium = '',
+        } = ctx.request.query
 
         const query = {}
         if (companyNum) query.companyNum = companyNum
         if (device) query.device = device
         if (area) query.area = area
         if (equipment) query.equipment = equipment
+        if (label) query.label = label
+        if (unreachable !== '2' && unreachable) query.unreachable = unreachable === '0' ? '否' : '是'
+        if (componentType) query.componentType = componentType
+        if (status) query.status = status
+        if (medium) query.medium = medium
 
-        const data = await componentCollection.find(query).toArray()
+        // 根据动静密封筛选
+        const q = {}
+        if (sealPointType) q.sealPointType = sealPointType
+        let componentTypes = await componentTypeCollection.find(q).toArray()
+        componentTypeArr = lodash.map(componentTypes, 'componentType')
+
+        let componentData = await componentCollection.find(query).toArray()
+        componentData = lodash.filter(componentData, c => { return componentTypeArr.includes(c.componentType) })
         
-        ctx.body = { code: 0 , message: '查询组件成功', data }
+        ctx.body = { code: 0 , message: '查询组件成功', data: componentData }
     } catch (err) {
         logger.log('componentInfoLedger异常:' + err, "error")
         ctx.body = { code: -1 , message: '查询组件失败' }
